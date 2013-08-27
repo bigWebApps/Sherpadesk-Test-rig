c17ed1848e613baca34367866f1a94ccb7a655ad
benpfohl <benpfohl@truenorthwebdesign.com>
2013-08-27 07:43:26 -0400

    most recent

---
+++ SherpaDeskTestRig/js/main.js
@@ -0,0 +1,9 @@
+﻿//Mobile show hide
+$('p.mobile-nav, header .sub-header ul li a').on('click', function () {
+    if ($('header .sub-header ul').is(":visible")) {
+        $('header .sub-header ul').removeAttr("style");
+    } else {
+        $('header .sub-header ul').slideDown();
+    }
+    
+})
